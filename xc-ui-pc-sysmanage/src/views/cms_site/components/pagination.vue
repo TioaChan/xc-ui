35321a910881490e6722b60e573bc6f733f99e90
TioaChan <xiaozhidiguo@gmail.com>
2020-08-21 18:32:44 +0800

    site manager

---
+++ xc-ui-pc-sysmanage/src/views/cms_site/components/pagination.vue
@@ -0,0 +1,53 @@
+<template>
+    <!--分页-->
+    <el-col :span="24" class="toolbar">
+        <el-pagination background layout="total,sizes,prev, pager, next" @current-change="handleChangePage" @size-change="handleSizeChange" :page-sizes="this.pageSizes" :page-size="this.params.size" :total="this.params.total" :current-page="this.params.page" style="float:right;">
+        </el-pagination>
+    </el-col>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                paginationData: {
+                    total: 0,
+                    page: 1, //页码
+                    size: 10, //每页显示个数
+                    siteId: 'findAll' //站点id
+                },
+                pageSizes: [10, 20, 30, 40], //每页可选显示个数
+            }
+        },
+        props: ["params"],
+        methods: {
+            handleChangePage(page) {
+                // console.log(page);
+                this.params.page = page;
+                return this.$emit("changePages");
+            },
+            handleSizeChange(size) {
+                this.params.size = size;
+                return this.$emit("changeSize");
+            }
+        },
+        mounted() {
+            this.paginationData = this.params; //来自父组件的数据 赋给 子组件
+        },
+        // 来自父组件的数据无法同步到子组件（第二次触发子组件以后）
+        // Vue父子组件通过prop异步传输数据踩坑：https://juejin.im/post/5b3cd8355188251b105ad14b
+        watch: {
+            params: {
+                deep: true,
+                handler() {
+                    this.paginationData = this.params;
+                    // console.log(nv + "==" + ov);
+                }
+            },
+        },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ xc-ui-pc-sysmanage/src/views/cms_site/components/table.vue
@@ -0,0 +1,137 @@
+<template>
+    <div>
+        <!--列表-->
+        <el-table :data="list" highlight-current-row v-loading="listLoading" :span="24" size="small">
+            <el-table-column type="index" label="#">
+            </el-table-column>
+            <el-table-column prop="siteName" label="站点名称" width="130">
+            </el-table-column>
+            <el-table-column prop="siteDomain" label="主机" width="230">
+            </el-table-column>
+            <el-table-column prop="sitePort" label="端口" width="80">
+            </el-table-column>
+            <el-table-column prop="siteWebPath" label="访问路径" width="80">
+            </el-table-column>
+            <el-table-column prop="sitePhysicalPath" label="物理路径">
+            </el-table-column>
+            <el-table-column prop="siteCreateTime" label="创建时间" :formatter="formatCreatetime" width="145">
+            </el-table-column>
+            <el-table-column label="编辑" width="80">
+                <template slot-scope="scope">
+                    <el-button size="mini" type="primary" @click="edit(scope.row)" disabled>编辑
+                    </el-button>
+                </template>
+            </el-table-column>
+            <el-table-column label="删除" width="80">
+                <template slot-scope="scope">
+                    <el-button size="mini" type="danger" @click="del(scope.row)" disabled>删除
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+
+    </div>
+</template>
+
+<script>
+    import * as cmsApi from '@/assets/api/cms_page.js'
+    import utilApi from '@/common/utils';
+    export default {
+        data() {
+            return {
+                // params: {
+                //     page: 1, //页码
+                //     size: 10, //每页显示个数
+                //     siteId: '' //站点id
+                // },
+                listLoading: false,
+                list: [],
+                // total: 0,
+            }
+        },
+        props: ["tableListData"],
+        methods: {
+            formatCreatetime(row) {
+                var createTime = new Date(row.siteCreateTime);
+                if (createTime) {
+                    return utilApi.formatDate(createTime, 'yyyy-MM-dd hh:mm:ss');
+                }
+            },
+            edit(row) {
+                // console.log(row)
+                return this.$emit("activeEditForm", row);
+                // this.$router.push({
+                //     path: '/cms/page/edit/' + pageId,
+                //     query: {
+                //         page: this.params.page,
+                //         siteId: this.params.siteId
+                //     }
+                // })
+            },
+            //删除
+            del(row) {
+                this.$confirm('确认删除该记录吗?', '提示', {
+                    type: 'warning'
+                }).then(() => {
+                    // console.log(row)
+                    this.listLoading = true;
+                    cmsApi.page_del(row.pageId).then((res) => {
+                        if (res.success) {
+                            this.$message.success("删除成功")
+                        } else {
+                            this.$message.error('删除失败');
+                        }
+                    });
+                }).finally(() => {
+                    this.listLoading = false;
+                    return this.$emit("flushTableData");
+                });
+            },
+            generateHtml(row) {
+                console.log(row)
+                // this.$router.push({
+                //     path: '/cms/page/html/' + id,
+                //     query: {
+                //         page: this.params.page,
+                //         siteId: this.params.siteId
+                //     }
+                // })
+            },
+            postPage(row) {
+                this.$confirm('确认发布该页面吗?', '提示', {}).then(() => {
+                    console.log(row)
+                    // this.listLoading = true;
+                    // cmsApi.page_postPage(id).then((res) => {
+                    //     if (res.success) {
+                    //         console.log('发布页面id=' + id);
+                    //         this.listLoading = false;
+                    //         this.$message.success('发布成功，请稍后查看结果');
+                    //     } else {
+                    //         this.$message.error('发布失败');
+                    //     }
+                    // });
+                }).catch(() => {
+
+                });
+            },
+        },
+        mounted() {
+            this.list = this.tableListData; //来自父组件的数据 赋给 子组件
+        },
+        // 来自父组件的数据无法同步到子组件（第二次触发子组件以后）
+        // Vue父子组件通过prop异步传输数据踩坑：https://juejin.im/post/5b3cd8355188251b105ad14b
+        watch: {
+            tableListData: {
+                deep: true,
+                handler() {
+                    this.list = this.tableListData;
+                    // console.log(nv + "==" + ov);
+                }
+            },
+        },
+    }
+</script>
+
+<style>
+
+</style>
