b776339637cb04633beda42d1d8259a71c9bc2eb
TioaChan <xiaozhidiguo@gmail.com>
2020-08-21 18:41:48 +0800

    template list

--- xc-ui-pc-sysmanage/src/views/cms_template/index.vue
+++ xc-ui-pc-sysmanage/src/views/cms_template/index.vue
@@ -1,13 +1,84 @@
 <template>
-    <div>TODO</div>
+    <div>
+
+        <!-- <siteSelector v-bind:siteData="siteData" @changeSite="handleFlushTableData"></siteSelector> -->
+
+        <!-- <el-button type="primary" size="small" @click="activeAddForm()">新增页面</el-button> -->
+
+        <!-- 数据 -->
+        <listTable v-bind:tableListData="tableListData"></listTable>
+        <!-- 分页按钮 -->
+        <!-- <pagination v-bind:params="params" @changePages="handleFlushTableData" @changeSize="handleFlushTableData"></pagination> -->
+
+        <!-- 新增标签弹层 -->
+        <!-- <div class="add-form">
+            <el-dialog title="新增页面" :visible.sync="dialogFormVisible">
+                <addForm @close="closeAddForm()"></addForm>
+            </el-dialog>
+        </div> -->
+        <!-- 编辑标签弹层 -->
+        <!-- <div class="edit-form">
+            <el-dialog title="编辑页面" :visible.sync="dialogFormVisible4Edit"> -->
+        <!-- 编辑套餐 需要传递
+                待编辑套餐的信息currentFormData
+                已勾选的检查组的id的数组currentCheckgroupIds 和
+                所有检查组currentTableData -->
+        <!-- <editForm @close="closeEditForm()" v-bind:currentFormData="currentFormData"></editForm>
+            </el-dialog>
+        </div> -->
+
+    </div>
 </template>
-
 <script>
-    export default {
+    import * as cms_templateApi from '@/assets/api/cms_template.js'
 
+    // import siteSelector from './components/siteselector'
+    import listTable from "./components/table"
+    // import pagination from "./components/pagination"
+    // import addForm from './components/add'
+    // import editForm from './components/edit'
+    export default {
+        components: {
+            listTable,
+            // pagination,
+            // siteSelector,
+            // addForm,
+            // editForm
+        },
+        data() {
+            return {
+
+                params: { //el-pagination 需要用到的数据
+                    total: 0, //总数
+                    page: 1, //页码
+                    size: 10, //每页显示个数
+                },
+
+                // siteData: { //site selector 需要用到的数据
+                //     siteId: 'findAll', //站点id
+                //     siteList: [] //站点列表
+                // },
+
+                tableListData: [], // listTable 需要用到的数据
+
+            }
+        },
+        methods: {
+            handleFlushTableData() { //查询全部页面信息
+                cms_templateApi.template_list().then(resp => {
+                    this.tableListData = resp.queryResult.list
+                })
+            },
+        },
+        created() {},
+        mounted() {
+            //默认查询页面
+            this.handleFlushTableData()
+        }
     }
 </script>
-
-<style>
-
+<style scpoe>
+    .toolbar {
+        margin-top: 1rem;
+    }
 </style>
